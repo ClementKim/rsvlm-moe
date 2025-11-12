68dc18b3ea8d6f15debedfc974f8c8cae4072727
Junsung - HPC <wnstjd123@gm.gist.ac.kr>
2025-11-12 13:23:31 +0000

    Update .gitignore and refactor rsvqa_base.sh for output paths; add main.py for dataset preparation and API testing

---
+++ llm/main.py
@@ -0,0 +1,62 @@
+import os
+import json
+
+from PIL import Image
+from dotenv import load_dotenv
+from tqdm import tqdm
+from google import generativeai as genai
+from google.generativeai import types
+
+def prepare_dataset():
+    # 데이터 준비
+    test_dir = "dataset/json/rsvqa/test.json"
+    with open(test_dir, 'r') as f:
+        test = json.load(f)
+
+    return test
+
+def gemini_test(api, question, img):
+    genai.configure(api_key=api)
+    model = genai.GenerativeModel('gemini-2.5-flash')
+    response = model.generate_content([question, img])
+
+    return response.text
+
+# def gemini_test(api,question, img):
+#     return 1
+
+def openai_test(api, question, img):
+    return 1
+
+def main():
+    load_dotenv()
+    gemini_api_key = os.getenv("GEMINI_API")
+    openai_api_key = os.getenv("OPENAI_API")
+
+    test_dataset = prepare_dataset()
+
+    gemini_response = {"low": {},
+                       "high": {}}
+    
+    openai_response = {"low": {},
+                       "high": {}}
+    for resol, value in test_dataset.items():
+        for question_id, question_set in tqdm(value.items()):
+            question = question_set['question']
+            img_path = question_set['image_path']
+
+            img = Image.open(img_path)
+
+            gemini_response[resol][question_id] = gemini_test(gemini_api_key, question, img)
+            # openai_response[resol][question_id] = openai_test(openai_api_key, question, img)
+
+        break
+
+    with open("results/gemini_results.json", "w") as f:
+        json.dump(gemini_response, f, indent = 4)
+
+    with open("results/openai_results.json", "w") as f:
+        json.dump(openai_response, f, indent = 4)
+
+if __name__ == "__main__":
+    main()
